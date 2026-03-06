39dfa0e6ef636083c22b3fc6e23b3a029beb963d
RodrigoPB <94554033+RodrigoPBarros@users.noreply.github.com>
2026-03-06 16:58:58 -0300

    py

---
+++ aulas/001/print.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+print("Olá, mundo!")
+print("Seu nome é:", nome)
+print("Sua idade é:", idade)
+print("Seu peso é:", peso)
