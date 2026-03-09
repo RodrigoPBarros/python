35ebf7989731f62800f57210a05698c9eeb8abdc
RodrigoPB <94554033+RodrigoPBarros@users.noreply.github.com>
2026-03-09 16:28:55 -0300

    commit

--- desafios/001/001.py
+++
@@ -1,6 +0,0 @@
-dia = int(input("Digite o dia que você nasceu: "))
-mes = (input("Digite o mês que você nasceu: "))
-ano = int(input("Digite o ano que você nasceu: "))
-
-print("=======DESAFIO 001=======")
-print(f"Você nasceu no dia {dia} de {mes} de {ano}. COrreto?")
---
+++ exercicios/004.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um número: "))
+dobro = n * 2
+triplo = n * 3
+raiz_quadrada = n ** 0.5
+print("======Resultado======")
+print(f"O dobro de {n} é {dobro}.")
+print(f"O triplo de {n} é {triplo}.")
+print(f"A raiz quadrada de {n} é {raiz_quadrada}.")
