0e883a251ccac93dfb1ab11c6797b17f17d2b9bc
RodrigoPB <94554033+RodrigoPBarros@users.noreply.github.com>
2026-03-19 16:30:45 -0300

    adad

--- desafios/060.py
+++ desafios/060.py
@@ -1,4 +1,10 @@
-from math import factorial
-n = int(input("Digite um número para calcualr seu Fatorial: "))
-f = factorial(n)
-print(f"O fatorial de {n} é {f}")
+n = int(input("Digite um número para calcular seu fatorial: "))
+c = n
+f = 1
+print(f"Calculando {n}\n", end="")
+while c > 0:
+    print(f"{c} ", end="")
+    print(f"x" if c > 1 else " = ", end = " ")
+    f *= c
+    c -= 1
+print(f)
